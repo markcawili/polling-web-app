3cdd3a94dba7b89243140e4fde1d491926d48051
Mark Cawili <mark.cawili@gmail.com>
2024-08-12 13:14:21 -0600

    Initialize middleware and first route

--- server.js
+++ server.js
@@ -2,5 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8383;
 
+//Middleware
+app.use(express.static('public'));
+
+//routes HTTP verbs
+app.post('/', function (req, res) {
+    res.status(200).send('Hi');
+})
+
+// GET POST PUT DELETE
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
