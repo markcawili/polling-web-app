0ad72650d9b09a651709620eb8265a5b13630df4
Mark Cawili <mark.cawili@gmail.com>
2024-08-13 12:09:02 -0600

    Implemented the JS for the database functions

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8383;
 
+/*The two functions we have are from the specified file*/
+const { readDb, writeDb } = require('./dbFunctions');
+
 //Middleware
 app.use(express.static('public'));
 //Expecting some json
@@ -10,9 +13,32 @@ app.use(express.json());
 //routes HTTP verbs
 app.post('/', function (req, res) {
     const { id, question, options } = req.body;
+
+    if (!id || !question || options.length === 0 ) {
+        res.status(400).send({status: "Error"})
+    }
+
     console.log(id, question, options);
+    const currentPolls = readDb();
+    /*Rewrite DB to include what is in there already and then add the new items*/
+    writeDb({
+        ...currentPolls,
+        [id]: {
+            question,
+            /*Takes the array of different options and returns object fromm accumulator and current value(option), initial 0 for the poll*/
+            options: options.reduce((acc, curr) => {
+                return {...acc, [curr]: 0 }
+            }, {})
+        }
+    })
     /*Do not forget to send status*/
     res.sendStatus(200);
+})
+
+app.get('/ids', (req, res) => {
+    const ids = readDb();
+    /*Send array of all ids to avoid overlap*/
+    res.status(200).send({ids: Object.keys(ids)} );
 })
 
 // GET POST PUT DELETE
