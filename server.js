7d7088ce22c1129682be2d6b237285ac7716c5c2
Mark Cawili <mark.cawili@gmail.com>
2024-08-21 12:35:33 -0600

    Added Firebase integration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8383;
+const {db} = require('./firebase');
 
 /*The two functions we have are from the specified file*/
 const { readDb, writeDb } = require('./dbFunctions');
@@ -11,17 +12,16 @@ app.use(express.static('public'));
 app.use(express.json());
 
 //routes HTTP verbs
-app.post('/', function (req, res) {
+app.post('/', async (req, res) => {
     const { id, question, options } = req.body;
 
     if (!id || !question || options.length === 0 ) {
         res.status(400).send({status: "Error"})
     }
 
-    const currentPolls = readDb();
-    /*Rewrite DB to include what is in there already and then add the new items*/
-    writeDb({
-        ...currentPolls,
+    const docRef = db.collection('polls').doc('polls');
+
+    const response = await docRef.set({
         [id]: {
             question,
             /*Takes the array of different options and returns object fromm accumulator and current value(option), initial 0 for the poll*/
@@ -29,15 +29,20 @@ app.post('/', function (req, res) {
                 return {...acc, [curr]: 0 }
             }, {})
         }
-    })
+        /*updates the items onto document*/
+    }, {merge: true});
+
     /*Do not forget to send status*/
     res.redirect('/' + id);
 })
 
-app.get('/ids', (req, res) => {
-    const ids = readDb();
+app.get('/ids', async (req, res) => {
+    const pollRef = db.collection('polls').doc('polls');
+    const data = await pollRef.get();
+
+    const polls = data.data();
     /*Send array of all ids to avoid overlap*/
-    res.status(200).send({ids: Object.keys(ids)} );
+    res.status(200).send({ids: Object.keys(polls)} );
 })
 
 // GET POST PUT DELETE
@@ -56,20 +61,38 @@ app.get('/:id', (req, res) => {
 })
 
 //For the data
-app.get('/data/:id', (req, res) => {
+app.get('/data/:id', async (req, res) => {
     const { id } = req.params;
+
     /*Read data of id*/
-    const data = readDb()[id];
-    res.status(200).send({data});
+    const pollRef = db.collection('polls').doc('polls');
+    const data = await pollRef.get();
+
+    const polls = data.data();
+
+    if (!Object.keys(polls).includes(id)) {
+        return res.redirect('/');
+    }
+    res.status(200).send({data: polls[id]});
 })
 
 //For the vote, sends and updates the DB
-app.post('/vote', (req, res) => {
+app.post('/vote', async (req, res) => {
     const { id, vote } = req.body;
-    const data = readDb();
+
+    const pollRef = db.collection('polls').doc('polls');
+    const polls = await pollRef.get();
+
+    const data = polls.data();
+
     data[id]['options'][vote] += 1;
-    writeDb(data);
-    res.sendStatus(200);
+
+    const docRef = db.collection('polls').doc('polls');
+
+    const response = await docRef.set({
+        ...data
+        /*updates the items onto document*/
+    }, {merge: true});    res.sendStatus(200);
 })
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
